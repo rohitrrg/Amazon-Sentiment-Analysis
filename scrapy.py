aa0ef0cfb2f4f39a61cfdc4c05a59b627abd96c0
rohitrrg <rohitgadhwar05@gmail.com>
2022-11-27 12:38:02 +0530

    update

--- scrapy.py
+++ scrapy.py
@@ -6,14 +6,14 @@ class AmazonReviews:
     def __init__(self, product_link):
         self.product_link = product_link
         self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36','referer':self.product_link}
-        page = requests.get(self.product_link, headers=self.header)
+        page = requests.get(self.product_link)
         self.soup = BeautifulSoup(page.content, features="html.parser")
         self.title = self.soup.findAll("span", {"class": "a-size-large product-title-word-break"})[0].text[7:]
         print(self.title)
 
     def searchreviewpage(self, review_link):
         url = str(self.product_link[:21])+review_link
-        page = requests.get(url,headers=self.header)
+        page = requests.get(url) # ,headers=self.header
         if page.status_code==200:
             return page
         else:
