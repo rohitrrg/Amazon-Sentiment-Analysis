eaa03c55b3683478cdf3a4240d7b03cbc843d8f1
rohitrrg <rohitgadhwar05@gmail.com>
2022-11-27 13:55:59 +0530

    update

--- scrapy.py
+++ scrapy.py
@@ -5,7 +5,8 @@ import re
 class AmazonReviews:
     def __init__(self, product_link):
         self.product_link = product_link
-        self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+        self.header = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
         page = requests.get(self.product_link, headers=self.header)
         self.soup = BeautifulSoup(page.content, features="html.parser")
         self.title = self.soup.findAll("span", {"class": "a-size-large product-title-word-break"})
